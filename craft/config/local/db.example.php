9a93b629bd706541ed40902f9af70b6fdcfcf4b2
Jimmy Lo <jimmylorunning@gmail.com>
2015-11-10 14:27:53 -0500

    Use custom multi-environment setup

---
+++ craft/config/local/db.example.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Local Database Info
+ *
+ * To setup:
+ * 1. Copy and rename this file to db.php
+ * 2. Update the array with your local database credentials
+ */
+
+return array(
+	'server' 		=> 'localhost',
+	'user' 			=> 'username',
+	'password' 	=> 'password',
+	'database' 	=> 'database_name'
+);
--- public/index.php
+++ public/index.php
@@ -3,16 +3,31 @@
 // Path to your craft/ folder
 $craftPath = '../craft';
 
+// Setup environment-friendly configs
+switch ($_SERVER['SERVER_NAME']) {
+
+	case 'atlantacyclingfestival.com' :
+		define('CRAFT_ENVIRONMENT', 'live');
+		break;
+
+	case '162.243.84.5' :
+		define('CRAFT_ENVIRONMENT', 'staging');
+		break;
+
+	case 'localhost' :
+		define('CRAFT_ENVIRONMENT', 'dev');
+		break;
+
+	default :
+		define('CRAFT_ENVIRONMENT', 'local');
+		break;
+}
+
 // Do not edit below this line
 $path = rtrim($craftPath, '/').'/app/index.php';
 
 if (!is_file($path))
 {
-	if (function_exists('http_response_code'))
-	{
-		http_response_code(503);
-	}
-
 	exit('Could not find your craft/ folder. Please ensure that <strong><code>$craftPath</code></strong> is set correctly in '.__FILE__);
 }
 
