1657c17ccf36ad9dc1f1b915cba7729617adc92d
jimmylorunning <jimmylorunning@gmail.com>
2015-12-08 10:41:50 -0500

    Events filter UI uses tabs between tracks and rides

--- public/assets/js/events.js
+++ public/assets/js/events.js
@@ -1,135 +1,125 @@
 $( document ).ready( function() {
 
-      isotopeOptions = {};
-      defaultOptions = {
-        filter: '*',
-        layoutMode: 'masonry'
-      };
+  isotopeOptions = {};
+  defaultOptions = {
+    filter: '*',
+    layoutMode: 'masonry'
+  };
 
   // init Isotope
-      var optionSets = $('#filters');
-      var container = $('.events_grid');
-      container.isotope(defaultOptions);
-      var isOptionLinkClicked = false;
+  var optionSets = $('#filters');
+  var container = $('.events_grid');
+  container.isotope(defaultOptions);
+  var isOptionLinkClicked = false;
 
-      $('.date-group').find('a').click(function(){
-        var $this = $(this);
-        changeSelectedLink( $this, '.date-group' );
-            // get href attr, remove leading #
-        var href = $this.attr('href').replace( /^#/, '' ),
-            // convert href into object
-            // i.e. 'filter=.inner-transition' -> { filter: '.inner-transition' }
-            option = $.deparam( href, true );
-        // apply new option to previous
-        $.extend( isotopeOptions, option );
-        // set hash, triggers hashchange on window
-        $.bbq.pushState( isotopeOptions );
-        isOptionLinkClicked = true;
-        return false;
-      });
+  /**** TABS *****/
+  $('#filtertabs a').click(function (e) {
+    e.preventDefault();
+    clicked = $(this).attr('name');
+    if (clicked == 'rides') {
+      var option = { 'track': '.ride' };
+      $.extend( isotopeOptions, option );
+      $.bbq.pushState( isotopeOptions );
+      $('#tracks').children().removeClass("selected");
+      isOptionClicked = true;
+    } else {
+      $.bbq.removeState( ['track'] );
+      $.bbq.removeState( ['difficulty'] );
+      $('#rides').children().removeClass("selected");
+      isOptionClicked = true;
+    }
+  })
 
-      $('.track-group').find('a').click(function(){
-        var $this = $(this);
-        changeSelectedLink( $this, '.track-group' );
-        var href = $this.attr('href').replace( /^#/, '' ),
-            option = $.deparam( href, true );
-        $.extend( isotopeOptions, option );
+  /**** FILTER BUTTON GROUPS ****/
+  $('.date-group').find('a').click(function(){
+    var $this = $(this);
+    changeSelectedLink( $this, '.date-group' );
+    var href = $this.attr('href').replace( /^#/, '' ),
+        option = $.deparam( href, true );
+    $.extend( isotopeOptions, option );
+    $.bbq.pushState( isotopeOptions );
+    isOptionLinkClicked = true;
+    return false;
+  });
 
-        console.log('option = ' + option['track']);
-        $.bbq.pushState( isotopeOptions );
-        if (option['track'] != '.ride') {
-          $.bbq.removeState(['difficulty']);
-          hideDifficulty();
-        } else {
-          showDifficulty();
-        }
-        isOptionLinkClicked = true;
-        return false;
-      });
+  $('#tracks').find('a').click(function(){
+    var $this = $(this);
+    changeSelectedLink( $this, '#tracks' );
+    var href = $this.attr('href').replace( /^#/, '' ),
+        option = $.deparam( href, true );
+    $.extend( isotopeOptions, option );
+    $.bbq.pushState( isotopeOptions );
+    isOptionLinkClicked = true;
+    return false;
+  });
 
-      $('.difficulty-group').find('a').click(function(){
-        var $this = $(this);
-        changeSelectedLink( $this, '.difficulty-group' );
-        var href = $this.attr('href').replace( /^#/, '' ),
-            option = $.deparam( href, true );
-        $.extend( isotopeOptions, option );
-        $.bbq.pushState( isotopeOptions );
-        isOptionLinkClicked = true;
-        return false;
-      });
+  $('#rides').find('a').click(function(){
+    var $this = $(this);
+    changeSelectedLink( $this, '#rides' );
+    var href = $this.attr('href').replace( /^#/, '' ),
+        option = $.deparam( href, true );
+    $.extend( isotopeOptions, option );
+    $.bbq.pushState( isotopeOptions );
+    isOptionLinkClicked = true;
+    return false;
+  });
 
-      function changeSelectedLink( $elem, $parentClass) {
-        // remove selected class on previous item
-        $elem.parents($parentClass).find('.selected').removeClass('selected');
-        // set selected class on new item
-        $elem.addClass('selected');
+  function changeSelectedLink( $elem, $parentClass) {
+    $elem.parents($parentClass).find('.selected').removeClass('selected');
+    $elem.addClass('selected');
+  }
+
+  var hashChanged = false;
+
+  /**** EVENT: on hashchange ****/
+  $(window).bind( 'hashchange', function( event ){
+    var hashOptions = window.location.hash ? $.deparam.fragment( window.location.hash, true ) : {};
+    // do not animate first call
+    var aniEngine = hashChanged ? 'best-available' : 'none';
+    var combinedOptions = hashOptions;
+    isotopeOptions = hashOptions;
+    if (typeof isotopeOptions['date'] == 'undefined') {
+      hashOptions['date'] = '*';
+    }
+    if (typeof isotopeOptions['track'] == 'undefined') {
+      hashOptions['track'] = '';
+    }
+    combinedOptions['filter'] = hashOptions['date'] + hashOptions['track'];
+    if (isotopeOptions['track'] == '.ride') {
+      if (typeof isotopeOptions['difficulty'] == 'undefined') {
+        hashOptions['difficulty'] = '';
       }
+      combinedOptions['filter'] = combinedOptions['filter'] + hashOptions['difficulty'];
+    }
 
-      function showDifficulty() {
-        $('.difficulty-group').show();
+    var options = $.extend( {}, defaultOptions, combinedOptions, { animationEngine: aniEngine } );
+    container.isotope( options );
+    $.bbq.pushState( combinedOptions);
+    isotopeOptions = hashOptions;
+
+    // if option link was not clicked
+    // then we'll need to update selected links
+    if ( !isOptionLinkClicked ) {
+      var hrefObj, hrefValue, $selectedLink;
+        for ( var key in options ) {
+          hrefObj = {};
+          hrefObj[ key ] = options[ key ];
+          // convert object into parameter string
+          // i.e. { filter: '.inner-transition' } -> 'filter=.inner-transition'
+          hrefValue = $.param( hrefObj );
+          // get matching link
+          if (hrefValue == 'track=.ride') {
+            $('#filtertabs a:last').tab('show'); 
+          }
+          $selectedLink = optionSets.find('a[href="#' + hrefValue + '"]');
+          console.log("hash sl = " + $selectedLink);
+          changeSelectedLink( $selectedLink , key + '-group');
+        }
       }
-
-      function hideDifficulty() {
-        $('.difficulty-group').hide();
-        changeSelectedLink($('#difficulty-all'), '.difficulty-group');
-      }
-
-      var hashChanged = false;
-
-      $(window).bind( 'hashchange', function( event ){
-        console.log('in hashchange');
-        // get options object from hash
-        var hashOptions = window.location.hash ? $.deparam.fragment( window.location.hash, true ) : {};
-        // do not animate first call
-        var aniEngine = hashChanged ? 'best-available' : 'none';
-        var combinedOptions = hashOptions;
-        isotopeOptions = hashOptions;
-        if (typeof isotopeOptions['date'] == 'undefined') {
-          hashOptions['date'] = '*';
-        }
-        if (typeof isotopeOptions['track'] == 'undefined') {
-          hashOptions['track'] = '';
-        }
-        combinedOptions['filter'] = hashOptions['date'] + hashOptions['track'];
-        if (isotopeOptions['track'] == '.ride') {
-          if (typeof isotopeOptions['difficulty'] == 'undefined') {
-            hashOptions['difficulty'] = '';
-          }
-          combinedOptions['filter'] = combinedOptions['filter'] + hashOptions['difficulty'];
-        }
-
-        // apply defaults where no option was specified
-        var options = $.extend( {}, defaultOptions, combinedOptions, { animationEngine: aniEngine } );
-        // apply options from hash
-        container.isotope( options );
-        $.bbq.pushState( combinedOptions);
-        // save options
-
-        isotopeOptions = hashOptions;
-
-        // if option link was not clicked
-        // then we'll need to update selected links
-        if ( !isOptionLinkClicked ) {
-          if (hashOptions['track'] != '.ride') {
-            hideDifficulty();
-          }
-          // iterate over options
-          var hrefObj, hrefValue, $selectedLink;
-          for ( var key in options ) {
-            hrefObj = {};
-            hrefObj[ key ] = options[ key ];
-            // convert object into parameter string
-            // i.e. { filter: '.inner-transition' } -> 'filter=.inner-transition'
-            hrefValue = $.param( hrefObj );
-            // get matching link
-            $selectedLink = optionSets.find('a[href="#' + hrefValue + '"]');
-            changeSelectedLink( $selectedLink , key + '-group');
-          }
-        }
-    
-        isOptionLinkClicked = false;
-        hashChanged = true;
-      })
-      // trigger hashchange to capture any hash data on init
-      .trigger('hashchange');
+  
+      isOptionLinkClicked = false;
+      hashChanged = true;
+    })
+    // trigger hashchange to capture any hash data on init
+    .trigger('hashchange');
 });
