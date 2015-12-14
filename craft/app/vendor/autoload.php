bac58c5908869a8400440de9372fd131ef0b5691
jimmylorunning <jimmylorunning@gmail.com>
2015-12-14 15:03:20 -0500

    Upgrade Craft to 2.5.2754

--- craft/app/vendor/autoload.php
+++ craft/app/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit4076df40583a55b0869fabb83e5c04b1::getLoader();
+return ComposerAutoloaderInit526b6b152a854e63a6b5040520054727::getLoader();
