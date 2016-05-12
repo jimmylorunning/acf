bd57b8b376ff9298bb23cbc881ec2c141579d7ed
Jimmy Lo <jimmylorunning@gmail.com>
2016-05-12 10:38:26 -0400

    Add endpoint to API for event photo

--- craft/config/elementapi.php
+++ craft/config/elementapi.php
@@ -7,8 +7,14 @@ return [
       'elementType' => 'Entry',
       'criteria' => ['section' => 'events'],
       'transformer' => function(EntryModel $entry) {
+        $photos = [];
+        foreach ($entry->photo as $p) {
+          $photos[] = $p->url;
+        }
+
         return [
           'title' => $entry->title,
+          'photos' => $photos,
           'starts' => $entry->startDateTime,
           'url' => $entry->url,
           'jsonUrl' => UrlHelper::getUrl("events/{$entry->id}.json"),
@@ -16,14 +22,21 @@ return [
       },
     ],
     'events/<entryId:\d+>.json' => function($entryId) {
-      return [
+
+     return [
         'elementType' => 'Entry',
         'criteria' => ['id' => $entryId],
         'first' => true,
         'transformer' => function(EntryModel $entry) {
+          $photos = [];
+          foreach ($entry->photo as $p) {
+            $photos[] = $p->url;
+          }
+
           return [
             'title' => $entry->title,
             'url' => $entry->url,
+            'photos' => $photos,
             'starts' => $entry->startDateTime,
             'startingAddress' => $entry->startingAddress,
             'startingLocation' => $entry->startingLocation,
