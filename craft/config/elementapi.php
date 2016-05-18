cd2b5eeb21f7a3269112b0850ea11b231d014674
Jimmy Lo <jimmylorunning@gmail.com>
2016-05-18 10:44:08 -0400

    Add latitude and longitude endpoints to API

--- craft/config/elementapi.php
+++ craft/config/elementapi.php
@@ -17,6 +17,8 @@ return [
           'photos' => $photos,
           'starts' => $entry->startDateTime,
           'track' => $entry->track,
+          'latitude' => $entry->latitude,
+          'longitude' => $entry->longitude,
           'url' => $entry->url,
           'jsonUrl' => UrlHelper::getUrl("events/{$entry->id}.json"),
         ];
@@ -41,6 +43,8 @@ return [
             'starts' => $entry->startDateTime,
             'startingAddress' => $entry->startingAddress,
             'startingLocation' => $entry->startingLocation,
+            'latitude' => $entry->latitude,
+            'longitude' => $entry->longitude,
             'track' => $entry->track,
             'difficulty' => $entry->rideDifficulty,
             'description' => ($entry->description ? (string) $entry->description : false),
